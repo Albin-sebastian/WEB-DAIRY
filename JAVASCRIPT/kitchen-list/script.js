8c76bc6c808f4592ec68bc736809655d751b84b4
Albin sebastian <albinsebastianmca@gmail.com>
2024-09-17 18:31:33 +0530

    local storage added

--- JAVASCRIPT/kitchen-list/script.js
+++ JAVASCRIPT/kitchen-list/script.js
@@ -1,75 +1,87 @@
-// Get the input field element
 let kitchenInput = document.getElementById
   ("kitchen-input");
 
-// Get the "Add" button element
 let addBtn = document.getElementById("add-btn");
 
-// Get the list element where we'll display the kitchen items
 let kitchenItemsList = document.getElementById("kitchen-items-list");
-// let trashBtn = document.createElement('i');
-// step-2
-// Define a function to add a new kitchen item
-const addKitchenItems = () => {
-  // Get the input data from the input field
-  let kitchenInputData = kitchenInput.value;
 
-  // Create a new list item element
-  let li = document.createElement('li');
-  let spanEl = document.createElement('span');
-  li.appendChild(spanEl);
-  // Set the text of the list item to the input data
-  spanEl.innerText = kitchenInputData;
+let kitchenInputData;
+let kitchenInputDataArray = [];
 
-
-
-  li.style.cssText = "animation-name:slideIn;";
-
-  // Add the list item to the list
-  kitchenItemsList.appendChild(li);
-  // Clear the input field
-  kitchenInput.value = "";
-
-  // Focus the input field so the user can start typing again
-  kitchenInput.focus();
-  // create trash button
-  let trashBtn = document.createElement('i');
-  trashBtn.classList.add('fa', 'fa-trash');
-  li.appendChild(trashBtn);
-
-  // create edit button
-  let editBtn = document.createElement('i');
-  editBtn.classList.add('fa', 'fa-edit');
-  li.appendChild(editBtn)
+const setLocalStorage = () => {
+  localStorage.setItem('kitchenInput', JSON.stringify(kitchenInputDataArray));
 }
-// delete item from kitchen list
+
+const getLocalStorage = () => {
+  if (localStorage.getItem('kitchenInput')) {
+    kitchenInputDataArray = JSON.parse(localStorage.getItem('kitchenInput'));
+    buildUi();
+  }
+
+}
+
+const buildUi = () => {
+  kitchenItemsList.textContent = "";
+  kitchenInputDataArray.forEach((item) => {
+
+    let li = document.createElement('li');
+    let spanEl = document.createElement('span');
+    li.appendChild(spanEl);
+    spanEl.innerText = item;
+    li.style.cssText = "animation-name:slideIn;";
+    kitchenItemsList.appendChild(li);
+    kitchenInput.value = "";
+    kitchenInput.focus();
+
+    let trashBtn = document.createElement('i');
+    trashBtn.classList.add('fas', 'fa-trash');
+    li.appendChild(trashBtn);
+
+    let editBtn = document.createElement('i');
+    editBtn.classList.add('fas', 'fa-edit');
+    li.appendChild(editBtn)
+  });
+}
+
+
+const addKitchenItems = () => {
+  kitchenInputData = kitchenInput.value;
+
+  kitchenInputDataArray.push(kitchenInputData);
+  setLocalStorage();
+
+  getLocalStorage();
+
+}
 const deleteKitchenItem = (e) => {
-  if (e.target.classList[0] == 'fa-trash') {
+  console.log(e.target.classList[1]);
+  if (e.target.classList[1] === 'fa-trash') {
     let item = e.target.parentElement;
     item.classList.add('slideOut');
     item.addEventListener('transitionend', () => {
       item.remove();
     })
-    // item.remove();
+
   }
 
 }
 
 const editKitchenItem = (e) => {
-  if (e.target.classList[1] == 'fa-edit');
-  {
+  console.log(e.target.classList[1]);
+  if (e.target.classList[1] === 'fa-edit') {
     let editValue = prompt('please add new text');
-    let item = e.target.parentElement;
-    console.log(item);
+    let items = e.target.parentElement;
 
-    let spanEl = item.querySelector('span');
+    let spanEl = items.querySelector('span');
     spanEl.innerText = editValue;
   }
+
 }
-// step-1
-// Add an event listener to the "Add" button to call the addKitchenItems function when clicked
+
 addBtn.addEventListener('click', addKitchenItems);
 
 kitchenItemsList.addEventListener('click', deleteKitchenItem);
 
 kitchenItemsList.addEventListener('click', editKitchenItem);
+
+getLocalStorage();
